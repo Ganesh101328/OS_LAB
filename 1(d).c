acb76ab64e9f7a014342fbe0330d2c00105baaca
Ganesh101328 <141500601+Ganesh101328@users.noreply.github.com>
2023-09-05 12:48:08 +0530

    Add files via upload

---
+++ 1(d).c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main(){
+int bt[20],wt[20],tat[20],p[20],bri[20],i,n,k,temp;
+float wtavg, tatavg;
+print("\n Enter the no.of processes");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+p[i]=i;
+printf("\nEnter burst time for process %d",i);
+scanf("%d%d" ,&bt[i], &pri[i]);
+}
+for(i=0;i<n;i++)
+for(k=i+1;k<n;k++)
+if(pri[i]>pri[k])
+{
+temp=p[i];
+p[i]=p[k];
+p[k]=temp;
+temp=bt[i];
+bt[i]=bt[k];
+bt[k]=temp;
+temp=pri[i];
+pri[i]=pri[k];
+pri[k]=temp;
+}
+wt[0]=wtavg=0;
+tat[0]=tatavg=bt[0];
+for(i=1;i<n;i++)
+{
+wt[i]=wt[i-1]+bt[i-1];
+tat[i]=tat[i-1]+bt[i];
+wtavg=wtavg+wt[i];
+tatavg=tatav=tat[i];
+}
+printf("\t Process \tBurstTime \tWaitingtime \tTurnaroundtime\n");
+for(i=0;i<n;i++)
+{
+printf("\n\t &d \t\t %d \t\t %d \t\t %d",i,bt[i],wt[i],tat[i],pri[i],p[i]);
+printf("\nAverage Waiting Time %f",wtavg/n);
+printf("\nAverage Turnaround Time %f",tatavg/n);
+}
+}
